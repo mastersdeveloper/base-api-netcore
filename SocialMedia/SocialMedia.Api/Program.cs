d913bf5d8ebc8ed87582bf040d1e50b502338136
David Cruz <dach_092@hotmail.com>
2021-08-31 19:09:22 -0500

    Subiendo cambios

--- SocialMedia/SocialMedia.Api/Program.cs
+++ SocialMedia/SocialMedia.Api/Program.cs
@@ -1,20 +1,56 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Net;
 
 namespace SocialMedia.Api
 {
     public class Program
     {
+        //https://github.com/lucassklp/Default.Architecture
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
+            //CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseKestrel((env, config) =>
+            {
+                var server = env.Configuration.GetSection("Server");
+
+                //Configura o HTTP
+                var http = server.GetSection("Http");
+                var httpPort = Convert.ToInt32(http.GetSection("Port").Value);
+                var httpIp = http.GetSection("ListenIp").Value;
+                config.Listen(IPAddress.Parse(httpIp), httpPort);
+
+                //Configura o HTTPS
+                //var https = server.GetSection("Https");
+
+                //if (http.GetValue<bool>("Enabled"))
+                //{
+                //    var httpsPort = Convert.ToInt32(https.GetSection("Port").Value);
+                //    var httpsIp = https.GetSection("ListenIp").Value;
+                //    var certificate = https.GetSection("Certificate").Value;
+                //    config.Listen(IPAddress.Parse(httpsIp), httpsPort, opt =>
+                //    {
+                //        var directory = $@"{Directory.GetCurrentDirectory()}\{certificate}";
+                //        var passwd = https.GetValue<string>("Password");
+                //        opt.UseHttps(directory, passwd);
+                //    });
+                //}
+            })
+            .UseStartup<Startup>()
+            .Build();
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
     }
 }
